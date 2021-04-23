0d1ebf6dba16d5132d82238059708ce73d5745a2
Naveen M K <naveen@syrusdark.website>
2021-04-23 18:01:48 +0530

    Add option to create language.def files
    Signed-off-by: Naveen M K <naveen@syrusdark.website>

--- texlive/file_creator.py
+++ texlive/file_creator.py
@@ -1,6 +1,7 @@
 import re
 import typing
 from pathlib import Path
+from string import Template
 
 from .logger import logger
 
@@ -104,6 +105,90 @@ def create_maps(
     temp_lines.sort()
     final_file = "\n".join(temp_lines)
     final_file.strip()
+    with filename_save.open("w", encoding="utf-8") as f:
+        f.write(final_file)
+        logger.info("Wrote %s", filename_save)
+    return filename_save
+
+
+def create_language_def(
+    pkg_infos: typing.Dict[
+        str, typing.Union[typing.Dict[str, typing.Union[str, list]]]
+    ],
+    filename_save: Path,
+):
+    """create_language_def This create language.def from the given
+    :attr:`pkg_infos`. :attr:`pkg_infos` can be is from
+    :func:`get_needed_packages_with_info`.
+
+    Parameters
+    ----------
+    pkg_infos
+        The dict of packages from
+    filename_save
+        The name of the file to save.
+    """
+    logger.info("Creating %s file", filename_save)
+    key_value_search_regex = re.compile(r"(?P<key>\S*)=(?P<value>[\S]+)")
+    final_file = ""
+
+    def parse_string(temp: str) -> typing.Dict[str, str]:
+        t_dict: typing.Dict[str, str] = {}
+        for mat in key_value_search_regex.finditer(temp):
+            if '"' not in mat.group("value"):
+                t_dict[mat.group("key")] = mat.group("value")
+        for i in [
+            "name",
+            "file",
+            "file_patterns",
+            "file_exceptions",
+            "lefthyphenmin",
+            "righthyphenmin",
+            "synonyms",
+        ]:
+            if i not in t_dict:
+                t_dict[i] = ""
+        return t_dict
+
+    for pkg in pkg_infos:
+        temp_pkg = pkg_infos[pkg]
+        if "execute" in temp_pkg:
+            temp = temp_pkg["execute"]
+            if isinstance(temp, str):
+                if "AddHyphen" in temp:
+                    final_file += f"% from {temp_pkg['name']}:\n"
+                    parsed_dict = parse_string(temp)
+                    final_file += Template(
+                        "\\addlanguage{$name}{$file}"
+                        "{}{$lefthyphenmin}{$righthyphenmin}\n",
+                    ).substitute(**parsed_dict)
+                    if parsed_dict["synonyms"]:
+                        synonyms = parsed_dict["synonyms"].split(",")
+                        for i in synonyms:
+                            parsed_dict["name"] = i
+                            final_file += Template(
+                                "\\addlanguage{$name}{$file}"
+                                "{}{$lefthyphenmin}{$righthyphenmin}\n",
+                            ).substitute(**parsed_dict)
+            else:
+                has_hypen = [True for each in temp if "AddHyphen" in each]
+                if has_hypen:
+                    final_file += f"% from {temp_pkg['name']}:\n"
+                for each in temp:
+                    if "AddHyphen" in each:
+                        parsed_dict = parse_string(each)
+                        final_file += Template(
+                            "\\addlanguage{$name}{$file}"
+                            "{}{$lefthyphenmin}{$righthyphenmin}\n"
+                        ).substitute(**parsed_dict)
+                        if parsed_dict["synonyms"]:
+                            synonyms = parsed_dict["synonyms"].split(",")
+                            for i in synonyms:
+                                parsed_dict["name"] = i
+                                final_file += Template(
+                                    "\\addlanguage{$name}{$file}"
+                                    "{}{$lefthyphenmin}{$righthyphenmin}\n"
+                                ).substitute(**parsed_dict)
     with filename_save.open("w", encoding="utf-8") as f:
         f.write(final_file)
         logger.info("Wrote %s", filename_save)
